44a01a54479bb786519d811e29eff5407e62e34c
Natali Zhylina <natalizhylina@Natalis-MacBook-Pro.local>
2019-11-29 16:47:39 +0200

    check for emptiness

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/go-chi/chi"
 	"io"
 	"log"
@@ -21,11 +22,12 @@ type LoginUser struct {
 	Password string
 }
 
+var View = jet.NewHTMLSet("./views")
+var root, _ = os.Getwd()
+
 func SignupGet(w http.ResponseWriter, r *http.Request) {
 	signupUser := SignupUser{}
 
-	View := jet.NewHTMLSet("./views")
-	root, _ := os.Getwd()
 	View = jet.NewHTMLSet(filepath.Join(root, "views"))
 
 	tmpl, err := View.GetTemplate("signup.jet.html")
@@ -47,9 +49,45 @@ func SignupGet(w http.ResponseWriter, r *http.Request) {
 }
 
 func SignupPost(w http.ResponseWriter, r *http.Request) {
+	signupUser := SignupUser{}
+
+	signupUser.Phone = r.FormValue("phone")
+	signupUser.Password = r.FormValue("pass")
+
+	fmt.Println(signupUser.Phone, signupUser.Password)
+
+	if signupUser.Phone == "" {
+		signupUser.Phone = "Empty phone"
+	}else {
+		signupUser.Phone = ""
+	}
+	if signupUser.Password == "" {
+		signupUser.Password = "Empty password"
+	}else {
+		signupUser.Phone = ""
+	}
+
+		View = jet.NewHTMLSet(filepath.Join(root, "views"))
+
+		tmpl, err := View.GetTemplate("signup.jet.html")
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+
+		body := &bytes.Buffer{}
+
+		err = tmpl.Execute(body, nil, signupUser)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+
+		w.Write(body.Bytes())
+
 }
 
-func LoginGet(w http.ResponseWriter, r *http.Request) {
+func LoginGet(w http.ResponseWriter, r *http.Request){
 	loginUser := LoginUser{}
 
 	View := jet.NewHTMLSet("./views")
@@ -74,7 +112,42 @@ func LoginGet(w http.ResponseWriter, r *http.Request) {
 	w.Write(body.Bytes())
 }
 
-func LoginPost(w http.ResponseWriter, r *http.Request) {
+func LoginPost(w http.ResponseWriter, r * http.Request) {
+	loginUser := LoginUser{}
+
+	loginUser.Phone = r.FormValue("phone")
+	loginUser.Password = r.FormValue("pass")
+
+	fmt.Println(loginUser.Phone, loginUser.Password)
+
+	if loginUser.Phone == "" {
+		loginUser.Phone = "Empty phone"
+	}else {
+		loginUser.Phone = ""
+	}
+	if loginUser.Password == "" {
+		loginUser.Password = "Empty password"
+	}else {
+		loginUser.Phone = ""
+	}
+
+	View = jet.NewHTMLSet(filepath.Join(root, "views"))
+
+	tmpl, err := View.GetTemplate("signup.jet.html")
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	body := &bytes.Buffer{}
+
+	err = tmpl.Execute(body, nil, loginUser)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	w.Write(body.Bytes())
 }
 
 func main() {
